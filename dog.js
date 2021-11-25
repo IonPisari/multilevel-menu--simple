d649c6f0e3065e4d2a987a2a39b802ed25d5040b
IonPisari <92115769+IonPisari@users.noreply.github.com>
2021-11-25 11:43:20 +0200

    dogyears-item

---
+++ dog.js
@@ -0,0 +1,9 @@
+const myAge = 24;//my years old
+let earlyYears = 2;//this value will change
+earlyYears *= 10.5;
+let laterYears = myAge - 2; //result when i substract 2 form myAge
+laterYears *= 4;
+console.log(earlyYears, laterYears);
+let myAgeInDogYears = Number(earlyYears) + Number(laterYears);
+let myName = 'Ion Pisari'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`)
