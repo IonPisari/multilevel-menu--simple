a220c7fc11ddcbe3417258349c68bd4ad751d298
IonPisari <92115769+IonPisari@users.noreply.github.com>
2021-12-16 14:52:16 +0200

    secretMessage

---
+++ secret-message.js
@@ -0,0 +1,29 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length);
+
+secretMessage.pop();
+
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'program');
+
+console.log(secretMessage.length);
+
+secretMessage.indexOf('easily');
+secretMessage[7] = 'right';
+
+console.log(secretMessage.length);
+
+secretMessage.shift();
+
+console.log(secretMessage.length);
+
+secretMessage.unshift('Programing');
+
+secretMessage.splice(6,5,'know');
+
+console.log(secretMessage.length);
+
+console.log(secretMessage.join(''));
+
