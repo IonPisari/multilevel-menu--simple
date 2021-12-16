c82de352ec537fd00b73eeee8f4ba739e392765f
IonPisari <92115769+IonPisari@users.noreply.github.com>
2021-12-16 13:29:16 +0200

    Update sleep-debt.js

--- sleep-debt.js
+++ sleep-debt.js
@@ -13,7 +13,7 @@ const getSleepHours = day => {
         case 'saturday':
             return 9;
         case 'sunday':
-            return 5;
+            return 8;
     }
 
 // if (day === 'monday') {
@@ -44,25 +44,38 @@ const getActualSleepHours = () => getSleepHours('monday') + getSleepHours('tuesd
 // const getActualSleepHours = () => 8 + 7 + 6 + 7 + 8 + 9 + 5 ; // this we can use for if/else statement not for switch
 // console.log(getActualSleepHours())
 
-// const getIdealSleepHours = () =>{
-//     let idealHours = 8;
-//     return idealHours * 7;
-//   }
-const getIdealSleepHours = (idealHours) => idealHours * 7;
+const getIdealSleepHours = () =>{
+    let idealHours = 8;
+    return idealHours * 7;
+  }
+// const getIdealSleepHours = (idealHours) => idealHours * 7;
   
 // console.log(getIdealSleepHours());
+// const calculateHours = () => {
+//     if (getActualSleepHours > getIdealSleepHours) {
+//         return getActualSleepHours() - getIdealSleepHours(); 
+//     }else {
+//         return getIdealSleepHours() - getActualSleepHours();
+//     }
+// }>>>>>>>> |
+//          sau 
+//      folosim metoda mai scurta 
+//           |
+const calculateHours = () => (getActualSleepHours > getIdealSleepHours) ? getActualSleepHours() - getIdealSleepHours():getIdealSleepHours() - getActualSleepHours();
+
+let res = calculateHours()
 
 const calculateSleepDebt = () =>{
     const actualSleepHours = getActualSleepHours();
-    const idealSleepHours = getIdealSleepHours(8);
-    let more = actualSleepHours - idealSleepHours;
-    let less = idealSleepHours - actualSleepHours;
+    const idealSleepHours = getIdealSleepHours();
+    // let more = actualSleepHours - idealSleepHours;
+    // let less = idealSleepHours - actualSleepHours;
     if (actualSleepHours === idealSleepHours) {
         console.log(`You have got the perfect amount of sleep:  ${actualSleepHours} `);
     }else if (actualSleepHours > idealSleepHours) {
-        console.log(`You got more sleep than needed:   ${more}`);
+        console.log(`You got more sleep than needed:   ${res}`);
     }else {
-        console.log(`You should get some rest, you have  ${less}  hours of sleep this week.`);
+        console.log(`You should get some rest, you have  ${res}  hours of sleep this week.`);
     }
 }
   
